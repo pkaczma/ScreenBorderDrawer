c75fc4e3083ec0810c3efc2c16d0a4a1aadf6d59
billthenut <pkaczmarek144@gmail.com>
2026-07-08 17:31:45 +0200

    Upload

---
+++ ScreenBorderDrawerDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿using ScreenBorderDrawer;
+
+Console.WriteLine("Hello, World!");
+ScreenBorderController screenBorderFormService = new();
+_ = screenBorderFormService.CreateBorderAsync(new(30,30), new(100,100), Color.Black, 5);
+Console.ReadLine();
